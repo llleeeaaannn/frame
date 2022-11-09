499998e3d8880e3decf399f2a7b6429f7bbc49d3
llleeeaaannn <piercefrank10@gmail.com>
2022-11-10 00:49:36 +0100

    Began project

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './style.css';
+import { slogans } from './variables';
+
+export default class Main {
+
+  constructor() {
+    this.value = 'HECK';
+  }
+
+  start() {
+
+  }
+
+}
---
+++ src/script.js
@@ -0,0 +1,7 @@
+import './style.css';
+import Main from './main.js';
+
+const main = new Main();
+main.start();
+
+console.log('Test');
