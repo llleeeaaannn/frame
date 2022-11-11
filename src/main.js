ac6c1250fe64327c024c4c068b397de456a6daf4
llleeeaaannn <piercefrank10@gmail.com>
2022-11-11 19:25:12 +0100

    Added further styling and began working on javascript

--- src/main.js
+++ src/main.js
@@ -14,7 +14,10 @@ export default class Main {
   }
 
   start() {
-    this.createImages()
+    this.createImages();
+    this.faqListener();
+    this.faqOptionsListener();
+    this.menuToggleListener();
   }
 
   createImages() {
@@ -44,4 +47,53 @@ export default class Main {
     galleryFourContainer.append(redPastaImage);
   }
 
+  faqListener() {
+    let faqs = document.getElementsByClassName('faq-outer');
+    faqs = [...faqs];
+    faqs.forEach((faq) => {
+      faq.addEventListener('click', () => {
+        if (faq.classList.contains('active')) {
+          faqs.forEach(element => element.classList.remove('active'));
+        } else {
+          faqs.forEach(element => element.classList.remove('active'));
+          faq.classList.add('active');
+        }
+      })
+    });
+  }
+
+  faqOptionsListener() {
+    const dietaryOption = document.getElementById('faq-dietary-option');
+    const bookingsOption = document.getElementById('faq-bookings-option');
+    const generalOption = document.getElementById('faq-general-option');
+    const dietaryQuestions = document.querySelector('.faq-dietary-container');
+    const bookingsQuestions = document.querySelector('.faq-bookings-container');
+    const generalQuestions = document.querySelector('.faq-general-container');
+    const allElements = [dietaryOption, bookingsOption, generalOption, dietaryQuestions, bookingsQuestions, generalQuestions];
+    dietaryOption.addEventListener('click', () => {
+      allElements.forEach(el => el.classList.remove('active'));
+      dietaryQuestions.classList.add('active');
+      dietaryOption.classList.add('active');
+    });
+    bookingsOption.addEventListener('click', () => {
+      allElements.forEach(el => el.classList.remove('active'));
+      bookingsQuestions.classList.add('active');
+      bookingsOption.classList.add('active');
+    });
+    generalOption.addEventListener('click', () => {
+      allElements.forEach(el => el.classList.remove('active'));
+      generalQuestions.classList.add('active');
+      generalOption.classList.add('active');
+    });
+  }
+
+  menuToggleListener() {
+    const menuBars = document.querySelectorAll('.menu-bar');
+    const menuToggle = document.getElementById('menu-toggle');
+    const menuContainer = document.getElementById('menu-container');
+    menuToggle.addEventListener('click', () => {
+      menuBars.forEach(bar => bar.classList.toggle('menu-selected'));
+      menuContainer.classList.toggle('active');
+    });
+  }
 }
